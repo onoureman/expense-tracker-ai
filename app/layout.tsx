28925551e3aa1411e7a9c0af0af2507974a85e08
onoureman <emansd8@gmail.com>
2025-11-10 00:02:05 +0200

    add clerk authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,21 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Fira_Mono } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
 
-const geistSans = Geist({
+// Sans-serif font
+const geistSans = Inter({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  weight: ["400", "500", "700"], // specify weights
 });
 
-const geistMono = Geist_Mono({
+// Monospace font
+const geistMono = Fira_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  weight: ["400", "500", "700"], // specify available weights
 });
 
 export const metadata: Metadata = {
@@ -23,12 +29,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Navbar />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
